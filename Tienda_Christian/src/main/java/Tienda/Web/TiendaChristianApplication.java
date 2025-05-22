834230cb642fdfe19d7e22e74c0609866f8bb533
Christian-Ort00 <cortega20867@ufide.ac.cr>
2025-05-21 19:14:36 -0600

    Commit 1
    Mi primer commit

---
+++ Tienda_Christian/src/main/java/Tienda/Web/TiendaChristianApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaChristianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaChristianApplication.class, args);
+	}
+
+}
